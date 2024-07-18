d665cd58969da7ac4c97bd0f0ac503cf9565dd9c
Eduar Ivan Bernal Benavides <evans1688@hotmail.com>
2024-07-18 16:09:55 -0500

    uso de templates

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
  */
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 Route::get('blog', function () {
      // consulta en base de datos
     $posts = [
@@ -18,10 +18,10 @@ Route::get('blog', function () {
     ];
 
     return view('blog', ['posts' => $posts]);
-});
+})->name('blog');
 Route::get('blog/{slug}', function ($slug) {
     // consulta en base de datos con el slug
     $post = $slug;
 
     return view('post', ['post' => $post]);
-});
+})->name('post');
