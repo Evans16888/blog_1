1b86758615c45e2549234270c343368f1b3ed73b
Eduar Ivan Bernal Benavides <evans1688@hotmail.com>
2024-07-17 20:08:59 -0500

    uso de rutas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
-
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+/**
+ * routes::get      |CONSULTAR
+ * routes::post     |GUARDAR
+ * routes::delete   |Eliminar
+ * routes::put      |ACTUALIZAR
+ */
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('blog/{slug}', function ($slug) {
+    return $slug;
+});
+Route::get('buscar', function (Request $request){
+    return $request->all();
 });
