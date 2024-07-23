77bdec2c30a3fad3f8c438fabc6e64e7c366578d
Eduar Ivan Bernal Benavides <evans1688@hotmail.com>
2024-07-22 20:38:53 -0500

    inicio de sesion

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,6 @@
 <?php
+
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\pageController;
 use Illuminate\Support\Facades\Route;
 /**
@@ -17,5 +19,17 @@ use Illuminate\Support\Facades\Route;
  Route::controller(pageController::class)->group(function(){
     Route::get('/', 'home')->name('home');
     Route::get('blog', 'blog')->name('blog');
-    Route::get('blog/{post:slug}', 'post')->name('post');
+    Route::get('blog/{zslug}', 'post')->name('post');
  });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
