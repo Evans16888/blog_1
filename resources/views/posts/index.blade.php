bf2690db357eb7c3da197b0bf1bd4c786106cb9a
Eduar Ivan Bernal Benavides <evans1688@hotmail.com>
2024-07-24 18:30:32 -0500

    listado

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,15 +1,34 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        	{{ __('Posts') }}
+       	</h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    Listado de publicaciones
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+
+                	<table class="mb-4">
+                        @foreach($posts as $post)
+                        <tr class="border-b border-gray-200 text-sm">
+                            <td class="px-6 py-4">{{ $post->title }}</td>
+                            <td class="px-6 py-4">
+                                <a href="#" class="text-indigo-600">Editar</a>
+                            </td>
+                            <td class="px-6 py-4">
+                            	Eliminar
+                            </td>
+                        </tr>
+                        @endforeach
+                    </table>
+
+                    {{ $posts->links() }}
+                    {{-- `{{ ->links() }}` está generando enlaces de paginación para la colección de
+                    publicaciones. Esto creará un conjunto de enlaces de paginación que permiten a los usuarios
+                    navegar a través de las diferentes páginas de publicaciones, mostrando típicamente un número
+                    determinado de publicaciones por página. --}}
                 </div>
             </div>
         </div>
