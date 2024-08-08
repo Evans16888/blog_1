9a9762140176ae4591e2af5a58a8f59a051a2573
Eduar Ivan Bernal Benavides <evans1688@hotmail.com>
2024-08-08 05:30:25 -0500

    Buscador

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -14,8 +14,10 @@
                 <a href="{{ route('home') }}">
                     <img src="{{ asset('images/logo.png') }}" class="h-10 w-auto"> <!-- Ajusta aquí el tamaño -->
                 </a>
-                <form action="">
-                    <input type="text" placeholder="Buscar">
+                <form action="{{ route('home') }}" class="flex-grow" method="GET">
+				    <input type="text" name="search" placeholder="Buscar" value="{{ request('search') }}"
+				    class="border border-gray-200 rounded py-2 px-4 w-1/2"
+				    >
                 </form>
             </div>
             @auth
