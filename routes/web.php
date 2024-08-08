b26cc796195076e6fda4132932fc4d02c10ab49c
Eduar Ivan Bernal Benavides <evans1688@hotmail.com>
2024-08-08 04:31:23 -0500

    pagina home

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,7 @@ use Illuminate\Support\Facades\Route;
 
  Route::controller(pageController::class)->group(function(){
     Route::get('/', 'home')->name('home');
-    Route::get('blog', 'blog')->name('blog');
-    Route::get('blog/{zslug}', 'post')->name('post');
+    Route::get('blog/{post::slug}', 'post')->name('post');
  });
 
 Route::get('/dashboard', function () {
